0908b8d130d41524f6253beb1133952753a24cf8
Aryan <aryan4p130202@gmail.com>
2021-12-30 19:28:22 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(MyApp());
 }
@@ -9,15 +9,18 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   
     return MaterialApp(
         
         themeMode: ThemeMode.light,
-        theme: ThemeData(primarySwatch: Colors.red), 
+        theme: ThemeData(
+          primarySwatch:Colors.deepPurple,
+          primaryTextTheme: GoogleFonts.latoTextTheme() 
+          ),
+         
         darkTheme: ThemeData(
           brightness: Brightness.dark,
         ),
-        initialRoute: "/home",
+        // initialRoute: "/home",
         routes: {
           "/": (context) => LoginPage(),
           "/home": (context) => HomePage(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,15 +6,22 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login Page", 
-        style: TextStyle (
-          fontSize: 20,
-          color: Colors.blue,
-          fontWeight: FontWeight.bold,
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset("assets/images/login_image.png",
+          fit: BoxFit.cover,
           ),
-        ),
-      ),
+          Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+
+          )
+          ],
+      )
     );
   }
 }
